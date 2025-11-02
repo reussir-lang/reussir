673577f2b408dae33d0f4add53f223e4978c897c
Schrodinger ZHU Yifan <i@zhuyi.fan>
2025-11-01 21:32:16 -0400

    [frontend] remove monad abstraction for type emission

--- frontend/reussir-codegen/src/Reussir/Codegen/Type/Emission.hs
+++ frontend/reussir-codegen/src/Reussir/Codegen/Type/Emission.hs
@@ -1,30 +1,10 @@
-{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Reussir.Codegen.Type.Emission where
 
-import Control.Monad.State.Strict qualified as S
-import Data.HashTable.IO qualified as H
-import Data.Text.Lazy qualified as T
-import Data.Text.Lazy.Builder qualified as TB
-import Reussir.Codegen.Context (Codegen, Emission (emit))
+import Reussir.Codegen.Context (Emission (emit))
 import Reussir.Codegen.Type.Data
-import Reussir.Codegen.Type.Record (InstantiatedName (..))
-import qualified Data.Text.Lazy.Builder as T
-
--- For type emission, we may run into recursive types. In this case, we need to keep track of what's already being emitted.
-newtype TypeEmissionState = TypeEmissionState (H.CuckooHashTable InstantiatedName ())
-
-newtype TypeEmissionT m a = TypeEmissionT {runTypeEmission :: S.StateT TypeEmissionState m a}
-  deriving (Functor, Applicative, Monad, S.MonadState TypeEmissionState, S.MonadIO, S.MonadTrans)
-
-startTypeEmission :: S.MonadIO m =>TypeEmissionT m a -> m a
-startTypeEmission te = S.liftIO H.new >>= \e -> fst <$> S.runStateT (runTypeEmission te) (TypeEmissionState e)
-
-type TypeEmission = TypeEmissionT Codegen
-
-statelessEmit :: Emission a => a -> TypeEmission T.Builder
-statelessEmit a = S.lift (emit a)
 
 instance Emission PrimitiveInt where
   emit PrimInt8 = pure "i8"
@@ -51,11 +31,3 @@ instance Emission Primitive where
 instance Emission Type where
   emit (TypePrim prim) = emit prim
   emit _ = error "Emission for non-primitive types not implemented yet"
-
-instance Emission InstantiatedName where
-  emit (InstantiatedName path args) = do
-    path' <- emit path
-    args' <- mapM emit args
-    if null args'
-      then pure $ path'
-      else pure $ path' <> "<" <> TB.fromLazyText (T.intercalate "," (map TB.toLazyText args')) <> ">"
