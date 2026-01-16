c990eede1dc8add7fd5570a4c3c21b1f73a768c4
Schrodinger ZHU Yifan <i@zhuyi.fan>
2026-01-16 10:16:51 -0500

    port variable operations into Core2

--- frontend/reussir-core2/src/Reussir/Core2/Semi/Variable.hs
+++ frontend/reussir-core2/src/Reussir/Core2/Semi/Variable.hs
@@ -1,29 +1,46 @@
 module Reussir.Core2.Semi.Variable where
 
-newVariable :: Identifier -> Maybe (Int64, Int64) -> Sem.Type -> Tyck Sem.VarID
-newVariable varName varSpan varType = do
-    st <- State.gets variableStates
-    let varID = Sem.VarID $ fromIntegral $ Seq.length st
-    let varDef = VarDef{varName, varSpan, varType}
-    State.modify $ \s ->
-        s
-            { variableStates = st Seq.|> varDef
-            }
-    nameMap <- State.gets variableNameMap
-    liftIO $ H.insert nameMap varName varID
+import Data.HashTable.IO qualified as H
+import Data.Int (Int64)
+import Data.Sequence qualified as Seq
+import Effectful (Eff, IOE, liftIO, (:>))
+import Effectful.Prim.IORef.Strict (Prim, readIORef', writeIORef')
+import Reussir.Core2.Types.Semi.Type (Type)
+import Reussir.Core2.Types.Semi.Variable (VarDef (..), VarTable (..))
+import Reussir.Core2.Types.UniqueID (VarID (..))
+import Reussir.Parser.Types.Lexer (Identifier)
+
+{- |
+    Create a new variable and return its ID
+-}
+newVariable ::
+    (IOE :> es, Prim :> es) =>
+    Identifier ->
+    Maybe (Int64, Int64) ->
+    Type ->
+    VarTable ->
+    Eff es VarID
+newVariable varName varSpan varType varTable = do
+    uniqueBinds <- readIORef' $ uniqueBindings varTable
+    let varID = VarID $ fromIntegral $ Seq.length uniqueBinds
+        varDef = VarDef{varName, varSpan, varType}
+        localBinds = localBindings varTable
+    writeIORef' (uniqueBindings varTable) $ uniqueBinds Seq.|> varDef
+    liftIO $ H.insert localBinds varName varID
     return varID
 
-getVarType :: Sem.VarID -> Tyck Sem.Type
-getVarType (Sem.VarID idx) = do
-    vars <- State.gets variableStates
-    let varDef = Seq.index vars (fromIntegral idx)
+-- | Get the type of a variable, assuming it exists
+getVarType :: (Prim :> es) => VarID -> VarTable -> Eff es Type
+getVarType (VarID idx) varTable = do
+    uniqueBinds <- readIORef' $ uniqueBindings varTable
+    let varDef = Seq.index uniqueBinds (fromIntegral idx)
     return $ varType varDef
 
-lookupVar :: Identifier -> Tyck (Sem.VarID, Sem.Type)
-lookupVar varName = do
-    nameMap <- State.gets variableNameMap
+-- | Lookup the locally effective variable with the given name
+lookupVar :: (IOE :> es, Prim :> es) => Identifier -> VarTable -> Eff es (Maybe (VarID, Type))
+lookupVar varName varTable = do
+    let nameMap = localBindings varTable
     liftIO (H.lookup nameMap varName) >>= \case
-        Just varID -> getVarType varID >>= \ty -> pure (varID, ty)
+        Just varID -> getVarType varID varTable >>= \ty -> pure (Just (varID, ty))
         Nothing -> do
-            reportError $ "Variable not found: " <> (unIdentifier varName)
-            return (Sem.VarID (-1), Sem.TypeBottom)
+            return Nothing
