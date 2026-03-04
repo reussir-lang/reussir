18873ccc51eabf143a3978c309045d4069fd7aae
Schrodinger ZHU Yifan <i@zhuyi.fan>
2026-03-04 09:24:31 -0500

    [test][NFC] address stackoverflow in windows and add HOAS nbe

--- tests/integration/frontend/nbe.rr.c
+++ tests/integration/frontend/nbe.rr.c
@@ -6,8 +6,8 @@ extern int32_t nbe_test_ffi(void);
 
 int main(void) {
   int32_t n = nbe_test_ffi();
-  if (n != 200000) {
-    fprintf(stderr, "FAIL: expected 200000, got %d\n", n);
+  if (n != 1000) {
+    fprintf(stderr, "FAIL: expected 1000, got %d\n", n);
     abort();
   }
   return 0;
